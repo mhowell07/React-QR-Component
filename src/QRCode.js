0f3cc87caf270ac6eee4c0dba9825d5c4930d2b5
Matt Howell <matthew.howell07@gmail.com>
2022-10-18 22:18:16 -0500

    Add QR Component

---
+++ src/QRCode.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Test extends React.Component {
+    
+    render() {
+        return(
+            <h1>Hello World</h1>
+        );
+    }
+}
+
+// export default Test;
+
